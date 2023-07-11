891a172d672f54d406e85d1b1c1aaa99933c5f98
Khinmon <monmyatthu2009@gmail.com>
2023-07-11 23:31:26 +0630

    add mongoose.

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI =
+  "mongodb+srv://mongo:mongo@nodetuts.dlelyda.mongodb.net/note-tuts?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
 // register view engine
 app.set("view engine", "ejs");
 
-// listen for requests
-app.listen(3000);
+// middleware & static files
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog 2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("64ad885a81031499ab549792")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 app.get("/", (req, res) => {
   const blogs = [
